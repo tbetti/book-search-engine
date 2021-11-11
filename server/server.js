ccb48ca0d26c877118398d23c0c4b43f09a79127
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-11-11 11:24:16 -0500

    Required ApolloServer, typeDefs, resolvers; added a server context

--- server/server.js
+++ server/server.js
@@ -1,30 +1,29 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const {typeDefs, resolvers} = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth.js');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+const PORT = process.env.PORT || 3002;
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
-server.applyMiddleware({app});
+server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
